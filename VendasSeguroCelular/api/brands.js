3ae9a9256ad58290af8723e5ea83c33efb07ac46
mvducatti <mvducatti@gmail.com>
2025-12-02 17:31:01 -0300

    mais celulares e memórias

--- VendasSeguroCelular/api/brands.js
+++ VendasSeguroCelular/api/brands.js
@@ -1,7 +1,9 @@
 export default function handler(req, res) {
   const brands = [
     { id: "APPLE", name: "Apple" },
-    { id: "SAMSUNG", name: "Samsung" }
+    { id: "SAMSUNG", name: "Samsung" },
+    { id: "MOTOROLA", name: "Motorola" },
+    { id: "XIAOMI", name: "Xiaomi" }
   ];
 
   res.status(200).json({
